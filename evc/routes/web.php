49073bb864d57298e749dde09b1db3c7fb771466
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-05-09 17:38:21 -0500

    Initial listings and database

--- evc/routes/web.php
+++ evc/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,32 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//all listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings',[
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all(),
+    ]);
 });
+
+//Single Listing
+Route::get('/listings/{id}', function ($id) {
+    return view ('listing', [
+        'listing' => Listing::find($id),
+    ]);
+});
+
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World</h1>', 200)
+//         ->header('Content-Type', 'text/plain')
+//         ->header('foo', 'bar');
+
+// });
+
+// // Route::get('/posts/{id}', function($id) {
+// //     return response('Post ' . $id)
+// // }) ->where('id','[0-9]+');
+
+// Route::get('/search', function(Request $request) {
+//     dd($request);
+// });

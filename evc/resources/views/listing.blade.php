e7adfbb46b1f2a9690a0e4dbd04e0753c7b9d00f
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-05-11 14:12:17 -0500

    Listing factory setup

--- evc/resources/views/listing.blade.php
+++ evc/resources/views/listing.blade.php
@@ -2,5 +2,14 @@
     {{$listing['title']}}
 </h2>
 <p>
+    Company: {{$listing['company']}}
+</p>
+<p>
+    Location: {{$listing['location']}}
+</p>
+<p>
+    Website: {{$listing['website']}}
+</p>
+<p>
     {{$listing['description']}}
 </p>

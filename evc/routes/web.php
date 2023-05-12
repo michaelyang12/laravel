5ba5406374d637c3a42685f822f6f3b53e36101c
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-05-11 19:56:01 -0500

    clockwork and job postings

--- evc/routes/web.php
+++ evc/routes/web.php
@@ -18,6 +18,12 @@ use App\Http\Controllers\ListingController;
 //all listings
 Route::get('/', [ListingController::class, 'index']);
 
+//Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
